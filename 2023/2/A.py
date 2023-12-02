29f563002e5faa2fb9916d764ab0d4cc7e10dace
pozuelom <martin.pozuelo@getground.co.uk>
2023-12-02 13:03:55 +0000

    Day 2

---
+++ 2023/2/A.py
@@ -0,0 +1,20 @@
+import sys
+from collections import defaultdict
+
+target_colors = {'red':12,'green':13,'blue':14}
+
+sum = 0
+for line in sys.stdin:
+    group, line = line.split(': ')
+    group = int(group.split()[1])
+    colors = defaultdict(int)
+    for subgroup in line.split('; '):
+        for duet in subgroup.split(', '):
+            n, color = duet.split()
+            n=int(n)
+            color = color
+            colors[color] = max(colors[color],n)
+    if all([colors[color]<=target_colors[color] for color in ['red','green','blue']]): 
+        sum += group
+
+print(sum)
---
+++ 2023/2/B.py
@@ -0,0 +1,17 @@
+import sys
+from collections import defaultdict
+
+sum = 0
+for line in sys.stdin:
+    group, line = line.split(': ')
+    group = int(group.split()[1])
+    colors = defaultdict(int)
+    for subgroup in line.split('; '):
+        for duet in subgroup.split(', '):
+            n, color = duet.split()
+            n=int(n)
+            color = color
+            colors[color] = max(colors[color],n)
+    sum += colors['red']*colors['green']*colors['blue']
+
+print(sum)
