6fdddc68d5db738c638c8ee5c35c922cad65c29e
pozuelom <martin.pozuelo@getground.co.uk>
2023-12-05 19:31:57 +0000

    Day 5

---
+++ 2023/5/A.py
@@ -0,0 +1,41 @@
+import sys
+from collections import defaultdict
+
+conversions = defaultdict(list)
+converts_to = {}
+
+def convert(type_, value):
+    assert type_ in conversions
+    for a, b, length in conversions[type_]:
+        if value >= a and value < a+length:
+            return converts_to[type_], b + value - a
+    return converts_to[type_], value
+
+for line in sys.stdin:
+    if ':' in line:
+        if line.startswith('seeds:'):
+            seeds = [int(v) for v in line.split(':')[1].split()]
+            continue
+        line = line.split()[0].split('-')
+        conversion = line[0]
+        converts_to[line[0]] = line[2]
+        continue
+    if not line.strip():
+        continue
+    dest, source, length = [int(v) for v in line.split()]
+    conversions[conversion].append((source,dest,length))
+
+
+def min_location(type_, value):
+    if type_ == 'location':
+        return value
+    return min_location(*convert(type_, value))
+
+# print(conversions)
+# print(converts_to)
+min_ = 10**100
+for seed in seeds:
+    value = min_location('seed', seed)
+    min_ = min(min_, value)
+print(min_)
+# 170017547 too low
---
+++ 2023/5/B.py
@@ -0,0 +1,61 @@
+import sys
+from collections import defaultdict
+
+conversions = defaultdict(list)
+converts_to = {}
+
+for line in sys.stdin:
+    if ':' in line:
+        if line.startswith('seeds:'):
+            seeds = [int(v) for v in line.split(':')[1].split()]
+            continue
+        line = line.split()[0].split('-')
+        conversion = line[0]
+        converts_to[line[0]] = line[2]
+        continue
+    if not line.strip():
+        continue
+    dest, source, length = [int(v) for v in line.split()]
+    conversions[conversion].append((source,dest,length))
+
+def convert(type_, value, remaining_length):
+    assert type_ in conversions
+    while 1:
+        for a, b, length in conversions[type_]:
+            if value >= a and value < a+length:
+                usable_length = length - (value - a)
+                yield converts_to[type_], b + value - a, min(remaining_length, usable_length)
+                if remaining_length <= usable_length:
+                    return # We used all the length
+                value, remaining_length = value+usable_length, remaining_length-usable_length # Continue with the rest of the length
+                break
+        else: # Couldn't find a conversion from value
+            a, b, length = min([(a, b, length_) for (a, b, length_) in conversions[type_] if a > value]+[(10**100, 10**100, 10**100)])
+            if a >= value + remaining_length: # No range accessible
+                yield converts_to[type_], value, remaining_length
+                return # We used all the length
+            yield converts_to[type_], value, a - value
+            value, remaining_length = a, remaining_length - (a - value)
+
+def min_location(type_, value, length):
+    if type_ == 'location':
+        return value
+    
+    min_ = 10**100
+    for type_, value, length in convert(type_, value, length):
+        min2 = min_location(type_, value, length)
+        min_ = min(min_, min2)
+
+    return min_
+    
+# print(conversions)
+# print(converts_to)
+min_ = 10**100
+for i in range(0, len(seeds), 2):
+    start = seeds[i]
+    length = seeds[i+1]
+
+    value = min_location('seed', start, length)
+    min_ = min(min_, value)
+print(min_)
+# 79004094
