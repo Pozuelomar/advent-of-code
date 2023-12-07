7496f70a86f392bc045e031d71ea2343563b89c6
pozuelom <martin.pozuelo@getground.co.uk>
2023-12-07 19:14:36 +0000

    Day 7

---
+++ 2023/7/B.py
@@ -0,0 +1,24 @@
+import sys
+from collections import Counter
+
+order = 'J23456789TQKA'
+
+hands = []
+for line in sys.stdin:
+    hand, bid = line.strip().split()
+    count = Counter(hand)
+    if 'J' in count:
+        other_cards = set(count.keys()) - {'J'}
+        if other_cards:
+            count[max(other_cards, key=count.get)] += count['J']
+            del count['J']
+    hands.append((
+        sorted(count.values(), reverse=True),
+        [order.index(c) for c in hand],
+        int(bid),
+    ))
+
+hands.sort()
+total = sum((i+1)*bid for (i, (hand,original,bid)) in enumerate(hands))
+print(total)
+# 248256639
