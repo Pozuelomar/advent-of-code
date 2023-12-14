3d26e1ff18e842a74a302b52c80cb8ee8f047d4b
pozuelom <martin.pozuelo@getground.co.uk>
2023-12-14 00:55:02 +0000

    Day 13

---
+++ 2023/13/A.py
@@ -0,0 +1,32 @@
+import sys
+
+arrays = [[]]
+for line in sys.stdin:
+    line = line.strip()
+    if line == "":
+        arrays.append([])
+        continue
+    arrays[-1].append(line)
+
+
+def check(arr, i):
+    return all(l1 == l2 for l1, l2 in zip(arr[i::-1], arr[i + 1 :]))
+
+
+sum_ = 0
+for array in arrays:
+    for i in range(len(array) - 1):
+        if check(array, i):
+            sum_ += (i + 1) * 100
+            break
+    else:
+        arr = list(zip(*array))
+        for i in range(len(arr) - 1):
+            if check(arr, i):
+                sum_ += (i + 1) * 1
+                break
+        else:
+            print("No match found")
+            print(*array, sep="\n")
+print(sum_)
+# 28895
